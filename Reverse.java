1e6810eeb9bada4ec950b3a87879f1a99cd05253
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2023-12-25 15:30:33 +0200

    update divisors and reverse

--- Reverse.java
+++ Reverse.java
@@ -2,8 +2,24 @@
  * Prints a given string, backward. Then prints the middle character in the string.
  * The program expects to get one command-line argument: A string.
  */
-public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
+public class Reverse 
+{
+	public static void main (String[] args)
+	{
+		String word = args[0];
+		int leng = word.length();
+		int mid = 1;
+		int middle = 0;
+		for (int i=leng-1; i>=0; i--)
+		{
+			if (mid == i)
+			{
+				middle = i;
+			}
+			System.out.print(word.charAt(i));
+			mid++;
+		}
+		System.out.println();
+		System.out.println("The middle character is :" + word.charAt(middle));
 	}
 }
